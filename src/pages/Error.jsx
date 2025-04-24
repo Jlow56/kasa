3a08997e260b3e99e52ccc69982b0fe32def800f
Jlow56 <jeanlougames@hotmail.com>
2025-04-24 12:11:57 +0200

    scss de la page error et du composant host

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import "./Error.css";
+import "./Error.scss";
 
 const ErrorPage = () => {
   return (
@@ -8,7 +8,7 @@ const ErrorPage = () => {
       <div className="not-found-container flex">
         <h2 className="not-found-h2">404</h2>
         <h3 className="not-found-h3">Oops! La page que vous demandez n'existe pas</h3>
-        <Link to="/">Retourner sur la page d'accueil</Link>
+        <Link to="/" className="not-found-link">Retourner sur la page d'accueil</Link>
       </div>
     </>
   );
